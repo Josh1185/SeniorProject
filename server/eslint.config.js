7ddf3236858f8f6fe9e64d6ee5a1de5b59ca4c0e
Josh Iehle <iehlejosh@gmail.com>
2026-09-15 20:31:44 -0400

    chore: Configure linting on server and client side

---
+++ server/eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist', 'src/generated']),
+  {
+    files: ['**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+]);
